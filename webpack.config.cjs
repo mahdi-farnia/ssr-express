30fc71f6863214c9f2e6adeb9efdb0984a20eb56
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2022-07-05 12:47:42 +0430

    replace webpack with rollup :))

---
+++ webpack.config.cjs
@@ -0,0 +1,40 @@
+const path = require('node:path');
+const CSSPlugin = require('mini-css-extract-plugin');
+
+const isDev = true;
+
+module.exports = ['login', 'register'].map((file) => ({
+  name: file,
+  mode: isDev ? 'development' : 'production',
+  entry: path.resolve('app', 'js', file + '.js'),
+  output: {
+    path: path.resolve('public', 'assets'),
+    filename: file + '.js'
+  },
+  devtool: isDev ? 'inline-source-map' : false,
+  watch: true,
+  watchOptions: {
+    ignored: ['node_modules', 'public', 'scripts', 'src', 'views', 'tailwind.config.cjs']
+  },
+  resolve: {
+    extensions: ['.js', '.mjs']
+  },
+  plugins: [new CSSPlugin({ filename: file + '.css' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          CSSPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+}));
