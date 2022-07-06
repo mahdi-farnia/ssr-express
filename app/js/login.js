048867a19509ba5604161d4d6af18356a3451826
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2022-07-07 01:13:54 +0430

    add vanta js wave background

--- app/js/login.js
+++ app/js/login.js
@@ -1,4 +1,18 @@
 import '../styles/global.css';
 import '../styles/login.css';
 
-console.log('Login Page!');
+VANTA.WAVES({
+  el: '#root',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x8878,
+  shininess: 41.0,
+  waveHeight: 10.0,
+  waveSpeed: 0.5,
+  zoom: 0.97
+});
